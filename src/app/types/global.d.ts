c95c379ed1ad4291c2cb3b4af15f708ba50ab645
Subin-mungmee <betapower00002@gmail.com>
2026-06-23 11:22:02 +0700

    2

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,8 @@
+declare module "*.css";
+declare module "*.module.css";
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.webp";
+declare module "*.svg";
